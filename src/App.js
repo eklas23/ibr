f70bf7b7782595b5913739c20cdf4f2ebd4d30c5
Eklas Uddin <eklasinfo@gmail.com>
2024-01-02 20:55:16 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const CountryInfoApp = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await axios.get('https://restcountries.com/v3.1/all');
+        setCountries(response.data);
+      } catch (error) {
+        console.error('Error fetching countries:', error);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
   return (
-    <div >
-      <Header></Header>
-      <Shop></Shop>
+    <div style={{textAlign: "center"}}>
+      <h1>Country Information Live</h1>
+      <ul>
+        {countries.map((country) => (
+          <p style={{fontSize: "20px"}} key={country.cca2}>
+            <h2>{country.name.common}</h2>
+            <img src={country.flags.svg} alt={`${country.name.common} Flag`} width="100" height="50" />
+            <p>Population: {country.population}</p>
+            <p>Area: {country.area} square kilometers</p>
+          </p>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default CountryInfoApp;
